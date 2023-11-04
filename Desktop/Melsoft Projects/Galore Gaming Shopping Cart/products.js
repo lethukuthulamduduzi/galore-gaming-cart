4dc716f66043ecf806fdf1455fed3d293cbada31
Lethukuthula Mduduzi <mduduzilethukuthula4@gmail.com>
2023-11-04 20:03:29 +0200

    Initial commit

---
+++ Desktop/Melsoft Projects/Galore Gaming Shopping Cart/products.js
@@ -0,0 +1,45 @@
+const products = [
+    {
+        id: 0,
+        name: "ThunderBlade X1",
+        price: 70000,
+        imageSRC: "https://media.takealot.com/covers_images/db6229cd75ba4b74b0dd79b3e2077079/s-zoom.file",
+        defaultQuantity: 1
+    },
+    {
+        id: 1,
+        name: "NovaStriker Zephyr",
+        price: 75000,
+        imageSRC: "https://media.takealot.com/covers_images/22d99e15b06e4b6093a8712eb20bc94b/s-zoom.file",
+        defaultQuantity: 1
+    },
+    {
+        id: 2,
+        name: "QuantumFury Titan",
+        price: 80000,
+        imageSRC: "https://media.takealot.com/covers_images/7b5237c783a5447ebe274989fe071dcd/s-zoom.file",
+        defaultQuantity: 1
+    },
+    {
+        id: 3,
+        name: "CyberCortex 9000",
+        price: 100000,
+        imageSRC: "https://media.takealot.com/covers_images/a5c4757bfdad487f8990428daa73c5b7/s-zoom.file",
+        defaultQuantity: 1
+    },
+    {
+        id: 4,
+        name: "VortexEdge Nemesis",
+        price: 80000,
+        imageSRC: "https://media.takealot.com/covers_images/dffe1d7ac002409d95f7b88cafc94635/s-zoom.file",
+        defaultQuantity: 1
+    },
+    {
+        id: 5,
+        name: "PhoenixPulse Omega",
+        price: 78000,
+        imageSRC: "https://media.takealot.com/covers_images/d22b5915ebb8494dae30986216e36582/s-zoom.file",
+        defaultQuantity: 1
+    }
+
+]
